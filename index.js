cbeadff4ce025383e4a13c5545ed1ff4f53a2725
James Prawdzik <sebaprawdzik@gmail.com>
2022-12-05 15:24:24 -0500

    1st update

---
+++ index.js
@@ -0,0 +1,30 @@
+const transporationMethodDiv = document.getElementById("transporation-method")
+selectVehicle = document.getElementById("dropdown-transportation-method").value
+
+
+
+const walkingImage = document.createElement("img")
+walkingImage.setAttribute(id="walking-image")
+walkingImage.src= vehicles[4].image
+walkingImage.alt="Cannot load image"
+const bicycleImage = document.createElement("img")
+bicycleImage.setAttribute(id="bicycle-image")
+bicycleImage.src= vehicles[3].image
+bicycleImage.alt="Cannot load image"
+const drivingImage = document.createElement("img")
+drivingImage.setAttribute(id="driving-image")
+drivingImage.src= vehicles[2].image
+drivingImage.alt="Cannot load image"
+const rocketImage = document.createElement("img")
+rocketImage.setAttribute(id="rocket-image")
+rocketImage.src= vehicles[1].image
+rocketImage.alt="Cannot load image"
+const speedOfLightImage = document.createElement("img")
+speedOfLightImage.setAttribute(id="speed-of-light-image")
+speedOfLightImage.src= vehicles[5].image
+speedOfLightImage.alt="Cannot load image"
+
+
+
+
+
